cee9023a25d79f38d5126591336e77f69ef374ef
JPMora89 <110551396+JPMora89@users.noreply.github.com>
2022-12-22 16:24:09 -0500

    Update app.py
    working version

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ def generate_form1():
     return render_template("form1.html", prompts=prompts)
 
 words = {}
-@app.route("/firststory")
+@app.route("/firststory", methods=['POST'])
 def show_first_story():
     """returns a story with user inputs"""
-    words['place'] = request.args.get('place')
-    words['noun'] = request.args.get('noun')
-    words['verb'] = request.args.get('verb')
-    words['adjective'] = request.args.get('adjective')
+    words['place'] = request.form['place']
+    words['noun'] = request.form['noun']
+    words['verb'] = request.form['verb']
+    words['adjective'] = request.form['adjective']
     
     newStory = first_story.generate(words)
     return render_template("story.html", story=newStory)
@@ -45,13 +45,13 @@ def generate_form2():
     prompts = second_story.prompts
     return render_template("form2.html", prompts=prompts)
 
-@app.route("/secondstory")
+@app.route("/secondstory", methods=['POST'])
 def show_second_story():
     """return a second story with user inputs"""
-    words['verb'] = request.args.get('verb')
-    words['noun'] = request.args.get('noun')
-    words['adverb'] = request.args.get('adverb')
-    words['food'] = request.args.get('food')
+    words['verb'] = request.form['verb']
+    words['noun'] = request.form['noun']
+    words['adverb'] = request.form['adverb']
+    words['food'] = request.form['food']
     
     newStory = second_story.generate(words)
     return render_template("story.html", story=newStory)
